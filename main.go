982a810f9a4e3528f45d8ed9d21f01e68178b858
Bernt-Johan Bergshaven <bbergshaven@gmail.com>
2015-12-08 17:26:44 +0100

    added pkg

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import "fmt"
+import "github.com/bbergshaven/hello/pkg/reverse"
 
 func main() {
 
 	fmt.Println("Hello Golang Trondheim Meetup!")
+
+	fmt.Println(reverse.ReverseString("Hello Golang Trondheim Meetup!"))
 
 }
